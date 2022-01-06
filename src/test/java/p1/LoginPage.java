c1e0c0118213e1b57f403cdf9fb6e9921074b354
Neelakanta-1991 <92379763+Neelakanta-1991@users.noreply.github.com>
2022-01-06 11:21:59 +0530

    Update LoginPage.java

--- src/test/java/p1/LoginPage.java
+++ src/test/java/p1/LoginPage.java
@@ -1,5 +1,11 @@
 package p1;
 
 public class LoginPage {
+  
+  	public static void main(String[] args) {
+		System.out.println("Login page");
+
+			System.out.println("login page get latest code");
+	}
 
 }
